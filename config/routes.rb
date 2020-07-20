d795962b4f1b4bd518a78ee8799f61378dacbf6e
noguchi_d <>
2020-07-20 11:23:30 +0900

    topコントローラー作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  get 'top/home'
   resources :books
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root 'test#top'
+  root 'top#home'
 end
