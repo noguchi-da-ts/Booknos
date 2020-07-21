de56c634a6f7931777d6438cbb3de0b3d4fa6336
noguchi_d <>
2020-07-21 10:26:22 +0900

    本の情報拡張

--- db/migrate/20200717035852_create_books.rb
+++ db/migrate/20200717035852_create_books.rb
@@ -2,9 +2,15 @@ class CreateBooks < ActiveRecord::Migration[5.2]
   def change
     create_table :books do |t|
       t.string :title, null: false
+      t.string :sub_title
       t.string :auther
-      t.string :isbn_code
+      t.string :isbn_code, null: false
       t.string :jan_code
+      t.string :image_url
+      t.text :caption
+      t.string :item_url
+
+
       t.date :released_at
 
       t.timestamps
