a17018e9ef26446d8d93e2e6d9c3244b8ad44c1c
noguchi_d <>
2020-07-22 16:09:04 +0900

    正規化とバリデーション追加

--- app/assets/javascripts/sp/book_register.js
+++ app/assets/javascripts/sp/book_register.js
@@ -1,18 +1,41 @@
 $(document).ready(function (){
-  console.log('本の登録機能');
+  console.log('本の登録機能JSを読み込みました');
 });
 
 $(document).on('click', '#isbn_submit_button', function (){
-  const $isbn = $('#isbn_form').val();
+  let isbn = $('#isbn_form').val();
+  isbn = normalizationIsbn(isbn);
 
-  console.log(`入力されたISBN: ${$isbn}`);
-  $.ajax({
-    type: "GET",
-    url: "/books/ajax_book_register",
-    data: { 
-      isbn_code: $isbn
-    }
-  }).done(function(html) {
-    $('#book_search_result').html(html);
-  });
+  if ( validateIsbn(isbn) ) {
+    console.log(`入力されたISBN: ${isbn}`);
+    $.ajax({
+      type: "GET",
+      url: "/books/ajax_book_register",
+      data: { 
+        isbn_code: isbn
+      }
+    }).done(function(html) {
+      $('#book_search_result').html(html);
+    });
+  } else {
+    $('#book_search_result')
+      .html("<strong>※ISBNコードのチェックに引っかかりました。もう一度確認してください。</strong>");
+  }
+  
 });
+
+function normalizationIsbn(isbn) {
+  isbn = isbn.replace(/\D/g, '');
+  isbn = isbn.replace(/[Ａ-Ｚａ-ｚ０-９]/g, function(s) {
+    return String.fromCharCode(s.charCodeAt(0) - 0xFEE0);
+  });
+  return isbn
+}
+
+function validateIsbn(isbn) {
+  if( isbn.length == 13 ) {
+    return true;
+  } else {
+    return false;
+  }
+}
