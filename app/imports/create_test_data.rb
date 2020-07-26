1a51e31869c751515c5e74d5e9258442b530cc02
野口大輔 <>
2020-07-26 13:07:29 +0900

    DB修正

--- app/imports/create_test_data.rb
+++ app/imports/create_test_data.rb
@@ -19,7 +19,7 @@ class CreateTestData
   ]
 
   @@test_borrows = [
-    {book_id: 1, user_id: 1, created_at: '2020-01-01 12:00:00'}, 
+    {book_id: 1, user_id: 1, is_returned: true, created_at: '2020-01-01 12:00:00'}, 
     {book_id: 2, user_id: 1, created_at: '2020-01-01 12:00:00'}
   ]
 
