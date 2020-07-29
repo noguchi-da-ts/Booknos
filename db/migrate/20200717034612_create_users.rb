21a2bca1b4f2cc885c092a7e97b3c1e7cdad23cc
野口大輔 <>
2020-07-29 11:48:11 +0900

    モデル修正

--- db/migrate/20200717034612_create_users.rb
+++ db/migrate/20200717034612_create_users.rb
@@ -3,6 +3,7 @@ class CreateUsers < ActiveRecord::Migration[5.2]
     create_table :users do |t|
       t.string :name, null: false, unique: true
       t.string :email, null: false, unique: true
+      t.string :password_digest
 
       t.timestamps
 
--- db/schema.rb
+++ db/schema.rb
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 2020_07_17_040645) do
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
     t.string "email", null: false
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["name", "email"], name: "index_users_on_name_and_email", unique: true
