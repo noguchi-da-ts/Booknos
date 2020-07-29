a44566f02889ff03f2d95c850599e0f6dcbf08d8
野口大輔 <>
2020-07-29 11:15:10 +0900

    デフォルトテスト削除

--- test/controllers/returns_controller_test.rb
+++ test/controllers/returns_controller_test.rb
@@ -1,9 +1,4 @@
 require 'test_helper'
 
 class ReturnsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get returns_index_url
-    assert_response :success
-  end
-
 end
