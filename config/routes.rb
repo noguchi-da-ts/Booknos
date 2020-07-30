5b020a46cb552a76d3427c297a88a3d8abf6e545
野口大輔 <>
2020-07-30 10:52:13 +0900

    session login

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   root 'top#home'
   get '/books/ajax_book_register'
   get '/signup', to: 'users#new'
   post '/signup',  to: 'users#create'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
 
   resources :borrows, only: [:new, :create]
   resources :returns, only: [:new, :create]
