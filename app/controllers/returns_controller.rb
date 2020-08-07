85e36c02001617d3501a3063dbdeadd6713b3f8b
野口大輔 <>
2020-08-07 23:54:30 +0900

    借りている本の一覧表示

--- app/controllers/returns_controller.rb
+++ app/controllers/returns_controller.rb
@@ -1,4 +1,28 @@
 class ReturnsController < ApplicationController
+  include SessionsHelper
+
+  before_action :logged_in_user, only: [:index]
+  before_action :correct_user,   only: [:index]
+
   def index
+    borrows = @user.borrows.where(is_returned: false).pluck(:book_id)
+    @now_rental_books = Book.where(id: borrows)
+  end
+
+  private
+
+   # ログイン済みユーザーかどうか確認
+   def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:alert] = "ログインしてください"
+      redirect_to login_url
+    end
+  end
+
+  # 正しいユーザーかどうか確認
+  def correct_user
+    @user = current_user
+    redirect_to(root_url) unless current_user?(@user)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   delete '/logout',  to: 'sessions#destroy'
 
   resources :borrows, only: [:new, :create]
-  resources :returns, only: [:new, :create]
+  resources :returns, only: [:index]
   resources :books
   resources :users
   # For details => https://docs.google.com/spreadsheets/d/15rZsM-0mllq8AsZ6BZhMPqradaahB711-fLVOczP2Xc/edit#gid=0
